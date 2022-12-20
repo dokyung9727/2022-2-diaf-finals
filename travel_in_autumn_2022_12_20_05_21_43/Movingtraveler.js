d62dfb5ceea6ddf868f83e3e2aab77961303db32
dokyung9727 <115522642+dokyung9727@users.noreply.github.com>
2022-12-20 20:25:54 +0900

    modify moving traveler 2

--- travel_in_autumn_2022_12_20_05_21_43/Movingtraveler.js
+++ travel_in_autumn_2022_12_20_05_21_43/Movingtraveler.js
@@ -17,13 +17,27 @@ class Movingtraveler{
     }
     
     checkatt2(){
-        if(this.pos.x >= 345){
-            this.vel.y = -0.2
+        if(this.pos.x >= 345 && this.pos.x < 400){
+            this.vel.y = -0.2;
+            this.tt = 255;
+          }
+          if(this.pos.x >= 400 && this.pos.x < 460){
+            this.vel.x = 0.55;
+            this.vel.y = -0.6;
+            this.tt = 255;
+          }
+          if(this.pos.y >= 170 && this.pos.y <= 245){
+            this.vel.x = -1;
+            this.vel.y = 0.15;
+            this.tt = 255;
+          }
+          if(this.pos.x <= 170){
+            this.tt = 0;
           }
     }
     
     show(){
-      tint(255,255);
+      tint(255,this.tt);
       image(traveler, this.pos.x, this.pos.y, this.w, this.w);
     }
   }
