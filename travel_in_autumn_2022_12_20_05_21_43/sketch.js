c5d6a8dd2fbc6ea9dc8c600c050832c56b1a7d4b
dokyung9727 <115522642+dokyung9727@users.noreply.github.com>
2022-12-20 20:34:42 +0900

    flight airplane again

--- travel_in_autumn_2022_12_20_05_21_43/sketch.js
+++ travel_in_autumn_2022_12_20_05_21_43/sketch.js
@@ -26,6 +26,11 @@ function draw() {
     travelermove.update();
     travelermove.show();
   }
+  if(travelermove.pos.x == 170 && travelermove.pos.y == 272){
+    airmove.pos.x = 100;
+    airmove.pos.y = 260;
+    airmove.tt = 255;
+  }
   place.update();
   place.show();
 }
