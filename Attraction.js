88f145465a80ce275d6bb4d6aa5903d0dcd4ab8d
dokyung9727 <115522642+dokyung9727@users.noreply.github.com>
2022-12-20 16:23:01 +0900

    Mouse to Circle Interaction

--- Attraction.js
+++ Attraction.js
@@ -1,8 +1,22 @@
 class Atraction {
     contructor(){
         this.pos = createVector(170, 290);
-        this.c = color(255,0,0);
+        this.c = color(255,0,0,150);
         this.w = 20;
+    }
+
+    update(){
+        this.checkmouse();
+    }
+
+    checkmouse(){
+        this.attdist = dist(this.pos.x, this.pos.y, mouseX, mouseY);
+        if(this.attdist < 20){
+            this.c = color(255, 0, 0);
+        } 
+        if(this.attdist > 20){
+            this.c = color(255,0,0,150);
+        }
     }
 
     show(){
