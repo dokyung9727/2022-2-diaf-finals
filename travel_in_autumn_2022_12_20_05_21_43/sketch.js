ceced988b2d57fc72899055d27100c63895040ab
dokyung9727 <115522642+dokyung9727@users.noreply.github.com>
2022-12-20 19:07:12 +0900

    moving airplane

--- travel_in_autumn_2022_12_20_05_21_43/sketch.js
+++ travel_in_autumn_2022_12_20_05_21_43/sketch.js
@@ -1,11 +1,25 @@
-let mapimg; 
+let mapimg;
+let airplane;
+
+let place;
+let airmove;
 
 function setup() {
   createCanvas(600, 600);
   mapimg = loadImage('busan map.jpg');
+  airplane = loadImage('airplane.png');
+
+  airmove = new Movingair();
+  place = new Attraction();
 }
 
 function draw() {
   background(220);
   image(mapimg,0,0, 600,600);
+  image(airplane, 50, 300, 50, 50);
+
+  airmove.update();
+  airmove.show();
+  place.update();
+  place.show();
 }
